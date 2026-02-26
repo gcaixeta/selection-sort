56bfe7462d59d937992f19f5d6a6fe869ef1a47f
Gustavo Caixeta Rosa <gstvcaixeta@gmail.com>
2026-02-26 08:55:59 -0300

    Added a generate_random_array function to the python impl

--- python/ssort.py
+++ python/ssort.py
@@ -1,7 +1,10 @@
-def find_smallest(array):
-    smallest = array[0]
-    smallest_index = 0
-    for i in range(1, len(array)):
+import random
+
+
+def find_smallest(array, start):
+    smallest = array[start]
+    smallest_index = start
+    for i in range(start + 1, len(array)):
         if array[i] < smallest:
             smallest = array[i]
             smallest_index = i
@@ -9,13 +12,35 @@ def find_smallest(array):
     return smallest, smallest_index
 
 
-def main():
-    array = [1, 5, 2, 5, 6, 432, 45, 6, 9, -23, -4213, -423]
+def generate_random_array(size, min, max):
+    randomArray = []
+    for _ in range(size):
+        randomNumber = random.randint(min, max)
+        randomArray.append(randomNumber)
+
+    return randomArray
+
+
+def selection_sort(array):
+    print("\n\nUnsorted array:")
+    print(array)
+    for i in range(len(array) - 1):
+        _, idx_smallest = find_smallest(array, i)
+        array[i], array[idx_smallest] = array[idx_smallest], array[i]
+
+    print("\n\nSorted array:")
     print(array)
 
-    for i in range(len(array) - 1):
-        _, idx_smallest = find_smallest(array)
-        array[i], array[idx_smallest] = array[idx_smallest], array[i]
+
+def main():
+    print("Ordering array of size 10")
+    selection_sort(generate_random_array(10, 0, 100))
+
+    print("Ordering array of size 100")
+    selection_sort(generate_random_array(100, 0, 100))
+
+    print("Ordering array of size 1000")
+    selection_sort(generate_random_array(1000, 0, 1000))
 
 
 if __name__ == "__main__":
