8a842089ba16ac6baa71eaa30a20e13bbb8ebf3c
Gustavo Caixeta Rosa <gstvcaixeta@gmail.com>
2026-02-25 22:08:49 -0300

    added randomArrayGenerator() method in Java

--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -1,9 +1,33 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> unsortedArray = new ArrayList<>(List.of(4, 8, 2, 3, 1 ,6 ,129, 10));
+        System.out.println("Sorting array with 10 elements.");
+        System.out.println();
+        sortArray(randomArrayGenerator(10, 0, 100));
+        System.out.println("Sorting array with 100 elements.");
+        System.out.println();
+        sortArray(randomArrayGenerator(100, 0, 100));
+        System.out.println("Sorting array with 1000 elements.");
+        System.out.println();
+        sortArray(randomArrayGenerator(1000, 0, 10000));
+    }
+
+    public static List<Integer> randomArrayGenerator(int size, int min, int max) {
+        Random random = new Random();
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            int number = random.nextInt(max - min + 1) + min;
+            list.add(number);
+        }
+
+        return list;
+    }
+
+    public static void sortArray(List<Integer> unsortedArray) {
         System.out.println("Unsorted array: " + unsortedArray);
         List<Integer> orderedArray = SelectionSort.sort(unsortedArray);
         System.out.println("Sorted array: " + orderedArray);
